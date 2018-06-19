fe10ab9790356e6e9447e6c09b8ad25b82999dc2
Richard Tom <tomrichard14@gmail.com>
2018-06-19 09:01:51 -0700

    1st half of thiinktube project

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict';
+/* global $*/
 
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3';
+const API_KEY = 'AIzaSyAh6bzjJbXfT9SOpaIk4RrIg5dpz51eeio';
+function getDataFromApi(searchTerm, callback) {
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    data: {
+      part: 'snippet',
+      key: API_KEY,
+      q: `${searchTerm} in:title`
+    },
+    dataType: 'json',
+    type: 'GET',
+    sucess: callback
+  };
+
+  $.ajax(settings);
+}
+
+function renderResult(result) {
+  return `
+  <div>
+    <h2>
+    <a class="js-result-title" href="${result.html_url}" target="_blank">${result.title}</a></h2>
+    <p>Description<span class="js-description">${result.description}</span></p>
+    <p>Thumbnail<span class="js-thumbnail-url">${result.thumbnail.url}</span></p>
+  </div>
+  `;
+}
