d94c661981e7ea61e5029d5737d5dc3a22525932
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-19 12:42:20 -0400

    initial page with youtube search results displayed

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 /* global $*/
 
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3';
-const API_KEY = 'AIzaSyAh6bzjJbXfT9SOpaIk4RrIg5dpz51eeio';
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyDtpcRxCtBx08yzU0hlQAV71vfNVbeo2n8';
 function getDataFromApi(searchTerm, callback) {
   const settings = {
     url: YOUTUBE_SEARCH_URL,
     data: {
       part: 'snippet',
       key: API_KEY,
-      q: `${searchTerm} in:title`
+      q: `${searchTerm}`
     },
     dataType: 'json',
     type: 'GET',
-    sucess: callback
+    success: callback
   };
 
   $.ajax(settings);
@@ -23,9 +23,26 @@ function renderResult(result) {
   return `
   <div>
     <h2>
-    <a class="js-result-title" href="${result.html_url}" target="_blank">${result.title}</a></h2>
-    <p>Description<span class="js-description">${result.description}</span></p>
-    <p>Thumbnail<span class="js-thumbnail-url">${result.thumbnail.url}</span></p>
+    <a class="js-result-title" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a></h2>
+    <p>Description: <span class="js-description">${result.snippet.description}</span></p>
   </div>
   `;
 }
+
+function displayYouTubeSearchData(data) {
+  console.log(data);
+  const results = data.items.map(item => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit(){
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    queryTarget.val('');
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
